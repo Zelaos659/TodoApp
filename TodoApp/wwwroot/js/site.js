04473e94b0f0c46c28ca2f0cebce6d6eec685a86
Никита Меньшиков <nikita10-01@outlook.com>
2023-08-08 18:09:41 +0300

    Рабочий фильтр задач

--- TodoApp/wwwroot/js/site.js
+++ TodoApp/wwwroot/js/site.js
@@ -5,11 +5,29 @@
 
 // Write your JavaScript code.
 
-var logBtn = document.getElementById("logBtn");
+//var logBtn = document.getElementById("logBtn");
 
-logBtn.onclick = function () {
+//logBtn.onclick = function () {
+//    var login = document.getElementById("loginInput");
+//    var pass = document.getElementById("passwordInput");
+//    alert(login.value);
+//}
+
+function Login() {
     var login = document.getElementById("loginInput");
     var pass = document.getElementById("passwordInput");
     alert(login.value);
 }
 
+function Show(a) {
+    allTasks = document.getElementById("AllTasks");
+    endedTasks = document.getElementById("EndedTasks");
+    if (a) {
+        allTasks.classList.add("visually-hidden");
+        endedTasks.classList.remove("visually-hidden");
+    }
+    else {
+        endedTasks.classList.add("visually-hidden");
+        allTasks.classList.remove("visually-hidden");
+    }
+}
